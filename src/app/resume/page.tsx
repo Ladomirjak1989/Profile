02433b250f966043cb8fdd98329d7cf2291006f3
Bettina <bettike18989@gmail.com>
2025-02-27 11:22:11 +0100

    Portfolio

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,14 @@
+import ResumePage from '@/components/Pages/ResumePage/ResumePage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+    <main >
+    < ResumePage/>
+    </main>
+  </div>
+  )
+}
+
+export default page
